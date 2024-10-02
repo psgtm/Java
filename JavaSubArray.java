5c7c2231cae1724af7ce5f145415775b5d2a9670
Parth Sarthi Gautam <85763617+psgtm@users.noreply.github.com>
2024-10-02 22:51:08 +0530

    Add files via upload

---
+++ JavaSubArray.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        scanner.close();
+        
+        int negativeSubarrayCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum < 0) {
+                    negativeSubarrayCount++;
+                }
+            }
+        }
+
+        System.out.println(negativeSubarrayCount);
+    }
+}
